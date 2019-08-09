b5804cdf974d3faf4fb519f2edb6750563664f01
伊藤凪冴 <nagisa.ito@e-grant.net>
2019-08-09 19:52:24 +0900

    import関連修正

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Axios from 'axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+Vue.config.productionTip = false
+// this.Axiosで全てのコンポーネントでAxiosが利用できるようになる
+Vue.prototype.Axios = Axios
+
+// Font Awesome関連
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

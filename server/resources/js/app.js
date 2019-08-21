a242c5f17c93a74837f625df13ea5c4a478f6239
伊藤凪冴 <nagisa.ito@e-grant.net>
2019-08-21 17:24:38 +0900

    bulma導入

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -3,8 +3,8 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
-require('./bootstrap');
+// Bootstrapは使用しない
+// require('./bootstrap');
 
 window.Vue = require('vue');
 
--- server/resources/js/bootstrap.js
+++ server/resources/js/bootstrap.js
@@ -9,8 +9,8 @@ window._ = require('lodash');
 try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap');
+    // Bootstrapは使用しない
+    // require('bootstrap');
 } catch (e) {}
 
 /**

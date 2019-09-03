17e0775649c714a08b8e6fcf7645bd3af0750020
伊藤凪冴 <nagisa.ito@e-grant.net>
2019-09-03 18:01:00 +0900

    パスワードリセット機能追加

--- server/resources/views/auth/passwords/email.blade.php
+++ server/resources/views/auth/passwords/email.blade.php
@@ -2,45 +2,45 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+    @if (session('status'))
+        <div class="notification is-primary">
+            <button class="delete"></button>
+            {{ session('status') }}
+        </div>
+    @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="column is-6 is-offset-3 box">
+        <div class="column mb-10">
+            <h4 class="title is-4">{{ __('パスワードリセット') }}</h4>
+        </div>
 
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+        <div class="column is-10 is-offset-1">
+            {{ Form::open(['route' => 'password.email']) }}
+                @csrf
+                <div class="field is-horizontal mb-30">
+                    <div class="field-label is-normal">
+                        <label class="label">メール</label>
+                    </div>
+                    <div class="field-body">
+                        <div class="field">
+                            <p class="control is-expanded has-icons-left">
+                                <input id="email" type="email" class="input @error('email') is-danger @enderror" autofocus
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="xxx.yyy@e-grant.net">
+                                <span class="icon is-small is-left"><i class="fas fa-envelope"></i></span>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                    <p class="help is-danger">{{ $message }}</p>
                                 @enderror
-                            </div>
+                            </p>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
+
+                <div class="field is-horizontal mb-10">
+                    <button type="submit" class="button is-primary is-medium is-fullwidth">{{ __('Send Password Reset Link') }}</button>
+                </div>
+
+            {{ Form::close() }}
         </div>
     </div>
 </div>

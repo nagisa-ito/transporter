f01c5815747658004ba5693b2fd9aa9da75d45db
伊藤凪冴 <nagisa.ito@e-grant.net>
2019-10-18 19:03:53 +0900

    axios導入

--- server/resources/js/app.js
+++ server/resources/js/app.js
@@ -7,6 +7,7 @@
 // require('./bootstrap');
 
 window.Vue = require('vue');
+window.Axios = require('axios');
 
 /**
  * The following block of code may be used to automatically register your
@@ -16,11 +17,12 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
- // components/以下のvueファイルを自動的にcomponent化してくれる
+// components/以下のvueファイルを自動的にcomponent化してくれる
+// TIPS: 新しくComponent作って反映されない時はだいたいキャッシュのせい
 const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
